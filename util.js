9b4d08572188480c1ec1e497a3da6041e70cad85
Alexandra <alexandrajn17@gmail.com>
2020-05-04 12:17:37 +0100

    simple unit test
    simple unit test on an isolated function using jest

--- util.js
+++ util.js
@@ -1,3 +1,11 @@
+// this file holds the functions being imported into app.js
+// uses node.js syntax because it's easier to test (easily compatible with Jest as it's native)
+// react & angular will require a more complex build workflow & additional packages
+// webpack is used to bundle it
+// these are the functions we will be testing
+
+// great for a simple unit test as it is fully isolated
+// takes an input and returns one output
 exports.generateText = (name, age) => {
   // Returns output text
   return `${name} (${age} years old)`;
---
+++ util.test.js
@@ -0,0 +1,19 @@
+// ENSURE TO CHANGE TEST IN package.json TO "jest"
+// to test run 'npm test' in the terminal, which will run all the tests in the file with jest
+
+// using destructuring syntax to require something from util.js
+// destructring = pulling some items out of the object and export it via the util.js file
+// this is the native way to import with Jest
+
+// below we are inmporting the generateText function, which will be globally available when we run our tests with Jest
+const { generateText } = require("./util");
+
+// arg1. describe the ouptut you are looking for within the test parentheses
+// arg1. anonymous function that jest will execute to run your test
+test("should output name and age", () => {
+  const text = generateText("Alex", 25);
+  // formulate the expectation, we pass in the thing we want to compare, in this case 'text'
+  // then we chain on helper functions to the expectable object
+  // toBe checks that a value is what you expect, using strict equality (===)
+  expect(text).toBe("Alex (25 years old)");
+});
